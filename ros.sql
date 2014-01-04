2f546af27e23fb9978f0c04fd42d8cd50a9d10b4
wangyingran <ispxin@vip.qq.com>
2014-01-04 10:58:39 +0800

    updata sql

--- ros.sql
+++ ros.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50090
 File Encoding         : 65001
 
-Date: 2014-01-02 13:49:50
+Date: 2014-01-04 10:58:13
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -36,7 +36,7 @@ INSERT INTO `ros_admin` VALUES ('1', 'admin', '21232f297a57a5a743894a0e4a801fc3'
 -- ----------------------------
 DROP TABLE IF EXISTS `ros_app`;
 CREATE TABLE `ros_app` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL auto_increment,
   `appid` char(50) character set utf8 NOT NULL,
   `category` int(11) NOT NULL,
   `type` char(50) character set utf8 NOT NULL,
@@ -46,15 +46,40 @@ CREATE TABLE `ros_app` (
   `height` int(11) NOT NULL,
   `url` char(50) character set utf8 NOT NULL,
   `isMax` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `description` longtext collate utf8_bin,
+  `author` char(255) collate utf8_bin NOT NULL,
+  `pubdate` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of ros_app
 -- ----------------------------
-INSERT INTO `ros_app` VALUES ('1', 'app_1', '0', 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1');
-INSERT INTO `ros_app` VALUES ('2', 'app_2', '0', 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1');
-INSERT INTO `ros_app` VALUES ('3', 'app_3', '0', 'app', 'QQ音乐', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1');
+INSERT INTO `ros_app` VALUES ('1', 'app_1', '1', 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1', '0', null, 'ispxin', '1386829188');
+INSERT INTO `ros_app` VALUES ('2', 'app_2', '1', 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1', '1', null, 'ispxin', '1386829188');
+INSERT INTO `ros_app` VALUES ('3', 'app_3', '5', 'app', 'QQ音乐', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1', '1', null, 'ispxin', '1386829188');
+
+-- ----------------------------
+-- Table structure for `ros_category`
+-- ----------------------------
+DROP TABLE IF EXISTS `ros_category`;
+CREATE TABLE `ros_category` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) character set utf8 NOT NULL,
+  `sort` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of ros_category
+-- ----------------------------
+INSERT INTO `ros_category` VALUES ('1', '网站', '2');
+INSERT INTO `ros_category` VALUES ('2', '娱乐', '5');
+INSERT INTO `ros_category` VALUES ('3', '游戏', '100');
+INSERT INTO `ros_category` VALUES ('4', '生活', '100');
+INSERT INTO `ros_category` VALUES ('5', '音乐', '100');
+INSERT INTO `ros_category` VALUES ('6', '工具', '100');
 
 -- ----------------------------
 -- Table structure for `ros_user`
