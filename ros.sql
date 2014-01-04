0a8ba9b7128418fe8ebedcbb684f9151960d57b2
ispxin <ispxin@vip.qq.com>
2014-01-05 00:08:43 +0800

    updata sql

--- ros.sql
+++ ros.sql
@@ -1,111 +1,140 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.0.8
+-- http://www.phpmyadmin.net
+--
+-- 主机: 127.0.0.1
+-- 生成日期: 2014-01-05 00:08:01
+-- 服务器版本: 5.6.12
+-- PHP 版本: 5.3.26
 
-Source Server         : mysql
-Source Server Version : 50090
-Source Host           : localhost:3306
-Source Database       : ros
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50090
-File Encoding         : 65001
 
-Date: 2014-01-04 10:58:13
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- 数据库: `ros`
+--
 
--- ----------------------------
--- Table structure for `ros_admin`
--- ----------------------------
-DROP TABLE IF EXISTS `ros_admin`;
-CREATE TABLE `ros_admin` (
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ros_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `ros_admin` (
   `id` int(11) NOT NULL,
   `user` char(50) NOT NULL,
   `password` char(50) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of ros_admin
--- ----------------------------
-INSERT INTO `ros_admin` VALUES ('1', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+--
+-- 转存表中的数据 `ros_admin`
+--
 
--- ----------------------------
--- Table structure for `ros_app`
--- ----------------------------
-DROP TABLE IF EXISTS `ros_app`;
-CREATE TABLE `ros_app` (
-  `id` int(11) NOT NULL auto_increment,
-  `appid` char(50) character set utf8 NOT NULL,
+INSERT INTO `ros_admin` (`id`, `user`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ros_app`
+--
+
+CREATE TABLE IF NOT EXISTS `ros_app` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `category` int(11) NOT NULL,
-  `type` char(50) character set utf8 NOT NULL,
-  `title` char(50) character set utf8 NOT NULL,
-  `icon` char(50) character set utf8 NOT NULL,
+  `type` char(50) CHARACTER SET utf8 NOT NULL,
+  `title` char(50) CHARACTER SET utf8 NOT NULL,
+  `icon` char(50) CHARACTER SET utf8 NOT NULL,
   `width` int(11) NOT NULL,
   `height` int(11) NOT NULL,
-  `url` char(50) character set utf8 NOT NULL,
+  `url` char(50) CHARACTER SET utf8 NOT NULL,
   `isMax` int(11) NOT NULL,
   `state` int(11) NOT NULL,
-  `description` longtext collate utf8_bin,
-  `author` char(255) collate utf8_bin NOT NULL,
+  `description` longtext COLLATE utf8_bin,
+  `author` char(255) COLLATE utf8_bin NOT NULL,
   `pubdate` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
 
--- ----------------------------
--- Records of ros_app
--- ----------------------------
-INSERT INTO `ros_app` VALUES ('1', 'app_1', '1', 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1', '0', null, 'ispxin', '1386829188');
-INSERT INTO `ros_app` VALUES ('2', 'app_2', '1', 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1', '1', null, 'ispxin', '1386829188');
-INSERT INTO `ros_app` VALUES ('3', 'app_3', '5', 'app', 'QQ音乐', '/ros/Public/Home/images/app/clover.png', '500', '300', 'http://www.wangyingran.com', '1', '1', null, 'ispxin', '1386829188');
+--
+-- 转存表中的数据 `ros_app`
+--
 
--- ----------------------------
--- Table structure for `ros_category`
--- ----------------------------
-DROP TABLE IF EXISTS `ros_category`;
-CREATE TABLE `ros_category` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) character set utf8 NOT NULL,
+INSERT INTO `ros_app` (`id`, `category`, `type`, `title`, `icon`, `width`, `height`, `url`, `isMax`, `state`, `description`, `author`, `pubdate`) VALUES
+(1, 1, 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', 500, 300, 'http://www.wangyingran.com', 1, 0, NULL, 'ispxin', 1386829188),
+(2, 1, 'app', '我的博客', '/ros/Public/Home/images/app/clover.png', 500, 300, 'http://www.wangyingran.com', 1, 1, NULL, 'ispxin', 1386829188),
+(3, 5, 'app', 'QQ音乐', '/ros/Public/Home/images/app/clover.png', 500, 300, 'http://www.wangyingran.com', 1, 1, NULL, 'ispxin', 1386829188);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ros_category`
+--
+
+CREATE TABLE IF NOT EXISTS `ros_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `sort` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
--- ----------------------------
--- Records of ros_category
--- ----------------------------
-INSERT INTO `ros_category` VALUES ('1', '网站', '2');
-INSERT INTO `ros_category` VALUES ('2', '娱乐', '5');
-INSERT INTO `ros_category` VALUES ('3', '游戏', '100');
-INSERT INTO `ros_category` VALUES ('4', '生活', '100');
-INSERT INTO `ros_category` VALUES ('5', '音乐', '100');
-INSERT INTO `ros_category` VALUES ('6', '工具', '100');
+--
+-- 转存表中的数据 `ros_category`
+--
 
--- ----------------------------
--- Table structure for `ros_user`
--- ----------------------------
-DROP TABLE IF EXISTS `ros_user`;
-CREATE TABLE `ros_user` (
-  `id` int(11) NOT NULL auto_increment,
-  `user` char(255) default NULL,
-  `password` char(255) default NULL,
+INSERT INTO `ros_category` (`id`, `name`, `sort`) VALUES
+(1, '网站', 2),
+(2, '娱乐', 5),
+(3, '游戏', 100),
+(4, '生活', 100),
+(5, '音乐', 100),
+(6, '工具', 100);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ros_user`
+--
+
+CREATE TABLE IF NOT EXISTS `ros_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` char(255) DEFAULT NULL,
+  `password` char(255) DEFAULT NULL,
   `desk1` longtext NOT NULL,
   `desk2` longtext NOT NULL,
   `desk3` longtext NOT NULL,
   `desk4` longtext NOT NULL,
   `desk5` longtext NOT NULL,
   `sortType` varchar(255) NOT NULL,
-  `regtime` int(11) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `regtime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
--- ----------------------------
--- Records of ros_user
--- ----------------------------
-INSERT INTO `ros_user` VALUES ('1', 'guest@ros.com', '21232f297a57a5a743894a0e4a801fc3', 'app_1', 'app_2', 'app_3', '', '', '', '1386829188');
-INSERT INTO `ros_user` VALUES ('2', 'ispxin@vip.qq.com', '21232f297a57a5a743894a0e4a801fc3', 'app_1,app_2', 'app_3', '', '', '', '', '1386829188');
-INSERT INTO `ros_user` VALUES ('3', 'bbb@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', '1386829188');
-INSERT INTO `ros_user` VALUES ('4', 'ccc@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', '1386829188');
-INSERT INTO `ros_user` VALUES ('5', 'ddd@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', '1386830447');
-INSERT INTO `ros_user` VALUES ('6', 'sss@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', '1386831562');
-INSERT INTO `ros_user` VALUES ('7', 'hhh@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', '1386832561');
+--
+-- 转存表中的数据 `ros_user`
+--
+
+INSERT INTO `ros_user` (`id`, `user`, `password`, `desk1`, `desk2`, `desk3`, `desk4`, `desk5`, `sortType`, `regtime`) VALUES
+(1, 'guest@ros.com', '21232f297a57a5a743894a0e4a801fc3', 'app_1', 'app_2', 'app_3', '', '', '', 1386829188),
+(2, 'ispxin@vip.qq.com', '21232f297a57a5a743894a0e4a801fc3', 'app_1,app_2', 'app_3', '', '', '', '', 1386829188),
+(3, 'bbb@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', 1386829188),
+(4, 'ccc@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', 1386829188),
+(5, 'ddd@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', 1386830447),
+(6, 'sss@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', 1386831562),
+(7, 'hhh@qq.com', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', 1386832561),
+(11, '111@qq.com', '5f4dcc3b5aa765d61d8327deb882cf99', '', '', '', '', '', '', 1388834639),
+(12, '222@qq.com', '5f4dcc3b5aa765d61d8327deb882cf99', '', '', '', '', '', '', 1388834867),
+(13, '333@qq.com', '96e79218965eb72c92a549dd5a330112', '', '', '', '', '', '', 1388834915),
+(14, '444@qq.com', '96e79218965eb72c92a549dd5a330112', '', '', '', '', '', '', 1388834954),
+(15, '555@qq.com', '96e79218965eb72c92a549dd5a330112', '', '', '', '', '', '', 1388835089);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
